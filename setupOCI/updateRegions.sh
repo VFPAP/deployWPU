51d6cabd765e1662c8b8d3904622bb17d9f51f2b
VFPAP <25368099+VFPAP@users.noreply.github.com>
2022-04-09 20:42:49 +0100

    Updated Ubuntu image to 2022.03.04

--- setupOCI/updateRegions.sh
+++ setupOCI/updateRegions.sh
@@ -3,7 +3,7 @@
 rm -f regions
 
 # Canonical-Ubuntu-20.04-Minimal-2021.10.19-0
-wget -q https://docs.oracle.com/en-us/iaas/images/image/cc6e4086-515b-4254-99ea-171acc5e7460/ -O regions
+wget -q https://docs.oracle.com/en-us/iaas/images/image/8f0cbe71-0f53-49f7-894c-0d27d895fc65/ -O regions
 
 for region in $(grep '\.image\.' regions|awk '{$1=$1};1'|rev|cut -c 6-|rev|cut -c 5-);do
     local=$(echo "$region"|cut -d. -f4)
