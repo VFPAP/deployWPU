58b59762f9f0b222bf503b558678546b05d16613
VFPAP <25368099+VFPAP@users.noreply.github.com>
2022-11-26 13:34:12 +0000

    Bump to Canonical-Ubuntu-20.04-Minimal-2022.10.31-0

--- setupOCI/updateRegions.sh
+++ setupOCI/updateRegions.sh
@@ -2,8 +2,8 @@
 
 rm -f regions
 
-# Canonical-Ubuntu-20.04-Minimal-2021.10.19-0
-wget -q https://docs.oracle.com/en-us/iaas/images/image/8f0cbe71-0f53-49f7-894c-0d27d895fc65/ -O regions
+# Canonical-Ubuntu-20.04-Minimal-2022.10.31-0
+wget -q https://docs.oracle.com/en-us/iaas/images/image/1755009b-9f4d-45a4-8bcc-3b916ba95797/ -O regions
 
 for region in $(grep '\.image\.' regions|awk '{$1=$1};1'|rev|cut -c 6-|rev|cut -c 5-);do
     local=$(echo "$region"|cut -d. -f4)
